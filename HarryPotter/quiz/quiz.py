522aaf12251e501e1a362e897b888023f8be09ea
Maggie Z <whereq@gmail.com>
2022-04-17 11:25:50 -0400

    Added one line in quiz.py

--- HarryPotter/quiz/quiz.py
+++ HarryPotter/quiz/quiz.py
@@ -1,6 +1,8 @@
 class Solution:
   def main(self, oriString: str) -> bool:
       print(oriString)
+      msg = "in the middle of an important lesson"
+      print(msg)
 
 if __name__ == '__main__':
   solution = Solution()
