63b70a9136856de0af961b00eaf17172e4e9e5c2
aria-mei <141102200+aria-mei@users.noreply.github.com>
2023-08-01 15:44:56 -0400

    aria test file donut
    trying to push through git desktop this time

--- Constellation/ariatest.py
+++ Constellation/ariatest.py
@@ -7,15 +7,17 @@ class Solution:
     my_window.bgcolor("DimGray")
     my_pen = turtle.Turtle()
     my_pen.speed("fastest")
-
+    #define a few colors
     color1 = "DeepPink1"
     color2 = "Yellow"
     color3 = "orange"
     my_pen.up()
+    #pick a starting point
     my_pen.goto(-60,-90)
     my_pen.down()
 
     #draw a little spirograph donut
+    #do this sixty times to make a fun pattern
     for i in range(60):      
       my_pen.color(color1)
       my_pen.right(10)
