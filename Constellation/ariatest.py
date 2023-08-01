8e15a0a537c1c026ab7eeb3798b80919e6197599
aria-mei <141102200+aria-mei@users.noreply.github.com>
2023-08-01 11:44:00 -0400

    Create ariatest.py
    test commit. draws a little donut.

---
+++ Constellation/ariatest.py
@@ -0,0 +1,37 @@
+import turtle
+
+#test file. 
+class Solution:
+  def draw(self):
+    my_window = turtle.Screen()
+    my_window.bgcolor("DimGray")
+    my_pen = turtle.Turtle()
+    my_pen.speed("fastest")
+
+    color1 = "DeepPink1"
+    color2 = "Yellow"
+    color3 = "orange"
+    my_pen.up()
+    my_pen.goto(-60,-90)
+    my_pen.down()
+
+    #draw a little spirograph donut
+    for i in range(60):      
+      my_pen.color(color1)
+      my_pen.right(10)
+      my_pen.forward(90)
+      my_pen.color(color2)
+      my_pen.right(10)
+      my_pen.forward(65)
+      my_pen.color(color3)
+      my_pen.left(86)
+      my_pen.forward(45)
+      
+    my_pen.hideturtle()
+
+if __name__ == '__main__':
+  solution = Solution()
+  solution.draw()
+
+  turtle.done
+  turtle.Screen().exitonclick()
