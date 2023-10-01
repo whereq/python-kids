293148b66689edfb44a9dfd8565e6dbad0e2cdb8
MM <maggie.jingxi.zhang@gmail.com>
2023-10-01 17:19:42 -0400

    Add cancer.py

---
+++ Constellation/cancer.py
@@ -0,0 +1,20 @@
+    import turtle
+
+    class Solution:
+        def draw(self):
+            my_window = turtle.Screen()
+            my_window.bgcolor("black")
+            my_pen = turtle.Turtle()
+            my_pen.speed("fastest")
+            my_pen.color("yellow")
+            my_pen.hideturtle()
+            use_guide = false
+
+            if use_guide:
+                self.drawCircle(my_pen,0,0,3,"yellow")
+
+
+                self.drawCircle(my_pen,100,0,10"yellow")
+                self.drawCircle(my_pen,200,0,20,"yellow")
+                
+
